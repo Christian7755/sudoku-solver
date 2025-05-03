1ba5652ca5c82db503c9fa57307dbe4a07e1e299
Christian7755 <inf23008@lehre.dhbw-stuttgart.de>
2025-05-03 14:43:01 +0200

    Fix: false presentation of rows and columns, false Numbers. Additional check of datatype and area of the input fields

--- frontend/src/app/components/sudoku/sudoku-grid/sudoku-grid.component.ts
+++ frontend/src/app/components/sudoku/sudoku-grid/sudoku-grid.component.ts
@@ -29,17 +29,19 @@ export class SudokuGridComponent implements OnInit {
 
   //access a certain cell
   setCell(row: number, col: number, value: number): void {
-    this.grid[col][row] = value;
+    this.grid[row][col] = value;
   }
 
   //Getting a certain cell
   getCell(row: number, col: number): number {
-    return this.grid[col][row];
+    return this.grid[row][col];
   }
 
   updateCell(event: { row: number, col: number, value: number}) {
-    this.grid[event.col][event.row] = event.value;
+    this.grid[event.row][event.col] = event.value;
 
     console.log("Sudoku-Grid: update cell col" + event.col + " and row " + event.row + " with value " + event.value );
   }
+
+  trackByIndex(index: number): number {return index; }
 }
